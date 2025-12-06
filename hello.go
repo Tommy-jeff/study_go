1e44202b73e62c72e3441f8c773f952bd569cad9
Tommy <thomasjefferson.dev@gmail.com>
2025-12-05 22:46:33 -0300

    feat: methods without error handling

--- hello.go
+++ hello.go
@@ -28,12 +28,18 @@ func main() {
 	eu := math.Eu
 	fmt.Printf("Valor da variável eu: %v\n", eu)
 
-	res, err := requests.GoogleRequest()
+
+	// HTTP Request with error handling
+	res1, err := requests.GoogleRequest()
 	if err != nil {
 		fmt.Printf("Error during googleRequest: %v\n", err)
 		return
 	}
+	fmt.Printf("Response status code: %v\n %v\n", res1.StatusCode, res1.Header)
 
-	fmt.Printf("Response status code: %v\n %v\n", res.StatusCode, res.Header)
+	// HTTP Request without error handling
+	res2, _ := requests.GoogleRequest()
+	
+	fmt.Printf("Response status code: %v\n %v\n", res2.StatusCode, res2.Header)
 
 }
