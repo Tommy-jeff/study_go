11f32a926ed6893e699905e9f5e27450c49b7d9d
Tommy <thomasjefferson.dev@gmail.com>
2025-12-05 21:22:52 -0300

    test

---
+++ hello.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"study_go/math"
+)
+
+func main() {
+	// a := "Oi"
+	// b := 24
+	// c := false
+	// d := 3.14
+	// e := `Olá, 
+	
+	
+	// Mundo!`
+	// fmt.Printf("\n%T\n%T\n%T\n%T\n%T\n", a, b, c, d, e)
+	// fmt.Printf("\n%v\n%v\n%v\n%v\n%v\n", a, b, c, d, e)
+
+	resultado := math.Add(10, 15)
+	fmt.Printf("\nResultado da soma: %v\n", resultado)
+
+}
