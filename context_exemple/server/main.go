16dc2b5c66375c3996e73de58d613067b6ae4db8
Tommy <thomasjefferson.dev@gmail.com>
2025-12-07 21:33:30 -0300

    feat: clienet added to interact with server

--- context_exemple/server/main.go
+++ context_exemple/server/main.go
@@ -7,20 +7,25 @@ import (
 )
 
 func main() {
+
+	// Configuração do servidor HTTP
 	http.HandleFunc("/", home)
 	http.ListenAndServe(":8080", nil)
-
 }
 
+// Manipulador para a rota raiz
 func home(w http.ResponseWriter, r *http.Request) {
+	// Obtém o contexto da requisição
 	ctx := r.Context()
 	log.Print("iniciou a chamada home")
 	defer log.Print("Finalizou a requisição")
 
 	select {
-	case <-time.After(5 * time.Second):
+	// Simula um processamento que leva 1 segundo
+	case <-time.After(time.Second):
 		log.Print("Requisição processada com sucesso")
 		w.Write([]byte("Hello, World!\n"))
+	// Se o contexto for cancelado (por exemplo, se o cliente desconectar), essa case será executada
 	case <-ctx.Done():
 		log.Print("Requisição cancelada pelo cliente")
 		http.Error(w, "Requisição cancelada pelo cliente", http.StatusRequestTimeout)
